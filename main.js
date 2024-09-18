fdfbe8be2d1a2d5151b6412c0f95cb1016dac593
Roman <romalasolution@icloud.com>
2024-09-18 03:07:46 +0300

    add product section

--- main.js
+++ main.js
@@ -17,6 +17,12 @@ function switchLanguage(lang) {
         document.getElementById('menu-item-contacts').textContent = translations[lang].menu.contacts;
         document.getElementById('menu-item-products').textContent = translations[lang].menu.products;
         document.getElementById('menu-item-about-company').textContent = translations[lang].menu.about_company;
+        document.getElementById('about-text-part1').textContent = translations[lang].hero_text.about.part1;
+        document.getElementById('about-text-part2').textContent = translations[lang].hero_text.about.part2;
+        document.getElementById('hero_header').textContent = translations[lang].name;
+        document.getElementById('products_description').textContent = translations[lang].subheaders.products.description;
+        document.getElementById('products_header').textContent = translations[lang].subheaders.products.label;
+
         if (lang === 'ua') {
             document.getElementById('switch-to-ua').classList.add('active');
         } else {
@@ -28,3 +34,22 @@ function switchLanguage(lang) {
 }
 
 document.addEventListener('DOMContentLoaded', loadTranslations);
+
+const carousels = document.querySelectorAll('.carousel');
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    carousels.forEach(carousel => {
+        carousel.setAttribute('data-animated', true);
+        const carouselTrack = document.querySelector('.carousel-track');
+        const carouselContent = Array.from(carouselTrack.children);
+        carouselContent.forEach((item) => {
+            const copy = item.cloneNode(true);
+            copy.setAttribute('aria-hidden', true)
+            carouselTrack.appendChild(copy)
+        })
+    })
+}
