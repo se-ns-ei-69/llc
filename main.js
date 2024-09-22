09365cc3e5bf925e4ba962564536e0355b1a5ca7
Roman <romalasolution@icloud.com>
2024-09-22 17:26:55 +0300

    content update

--- main.js
+++ main.js
@@ -116,7 +116,6 @@ function switchLanguage(lang) {
     document.getElementById('switch-to-ua').classList.remove('active');
     document.getElementById('switch-to-en').classList.remove('active');
     if (translations[lang]) {
-//menu links
         document.getElementById('menu-item-contacts').textContent = translations[lang].menu.contacts;
         document.getElementById('menu-item-products').textContent = translations[lang].menu.products;
         document.getElementById('menu-item-about-company').textContent = translations[lang].menu.about_company;
@@ -134,7 +133,6 @@ function switchLanguage(lang) {
 
         document.querySelectorAll('.contacts_header').forEach(node => node.textContent = translations[lang].subheaders.contacts.label );
         document.getElementById('contacts_description').textContent = translations[lang].subheaders.contacts.description;
-//contacts
         document.querySelectorAll('.sales_label').forEach(node => node.textContent = translations[lang].contacts.staff.sales.label)
         document.querySelectorAll('.sales_name').forEach(node => node.textContent = translations[lang].contacts.staff.sales.name)
 
@@ -144,21 +142,20 @@ function switchLanguage(lang) {
         document.getElementById('commercial_label').textContent = translations[lang].contacts.staff.commercial.label;
         document.getElementById('commercial_name').textContent = translations[lang].contacts.staff.commercial.name;
 
-//table head
+        document.getElementById('director_label').textContent = translations[lang].contacts.staff.director.label;
+        document.getElementById('director_name').textContent = translations[lang].contacts.staff.director.name;
+
         document.getElementById('tHeader_number').textContent = translations[lang].table.thNumber;
         document.getElementById('tHeader_name').textContent = translations[lang].table.thName;
         document.getElementById('tHeader_size').textContent = translations[lang].table.thSize;
 
         document.getElementById('pdfButton').textContent = translations[lang].pdfButton.label;
 
-//address
         document.getElementById('label1').textContent = translations[lang].address.label1;
         document.getElementById('label2').textContent = translations[lang].address.label2;
-        document.getElementById('label3').textContent = translations[lang].address.label3;
 
         document.getElementById('field1').textContent = translations[lang].address.field1;
         document.getElementById('field2').textContent = translations[lang].address.field2;
-        document.getElementById('field3').textContent = translations[lang].address.field3;
 
         if (lang === 'ua') {
             document.getElementById('switch-to-ua').classList.add('active');
@@ -190,18 +187,6 @@ function addAnimation() {
         })
     })
 }
-
-// function changeLanguage(lang) {
-//     const table = document.getElementById('products_tbody');
-//     const rows = table.querySelectorAll('tr');
-//
-//     tableContentArray[lang].forEach((rowData, rowIndex) => {
-//         const cells = rows[rowIndex].querySelectorAll('td');
-//         rowData.forEach((cellData, cellIndex) => {
-//             cells[cellIndex].textContent = cellData;
-//         });
-//     });
-// }
 
 const burgerMenu = document.getElementById('burger-menu');
 const navigation = document.getElementById('nav');
